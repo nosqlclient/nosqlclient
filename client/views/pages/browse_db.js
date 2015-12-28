93b96da46f52a8baaf0e656a6bda502f8449a8ca
Sercan <ozdemirsercan27@gmail.com>
2015-12-28 16:20:26 +0100

    ace editor and switch-view added

--- client/views/pages/browse_db.js
+++ client/views/pages/browse_db.js
@@ -1,13 +1,17 @@
 /**
  * Created by RSercan on 26.12.2015.
  */
-var editor;
+var jsonEditor;
+var jsonEditorOptions = {
+    mode: "tree",
+    search: true
+};
 
 getEditor = function () {
     if ($('.jsoneditor').length == 0) {
-        editor = new JSONEditor(document.getElementById("jsoneditor"));
+        jsonEditor = new JSONEditor(document.getElementById("jsoneditor"), jsonEditorOptions);
     }
-    return editor;
+    return jsonEditor;
 }
 
 Template.browseDB.helpers({
@@ -17,19 +21,56 @@ Template.browseDB.helpers({
 });
 
 Template.browseDB.events({
+    'click #btnSwitchView': function (e) {
+        var jsonView = $('#divJsonEditor');
+        var aceView = $('#divAceEditor');
+
+        if (jsonView.css('display') == 'none') {
+            aceView.hide();
+            jsonView.show('slow');
+        } else {
+            jsonView.hide();
+            aceView.show('slow');
+        }
+
+    },
+
+    'keypress #inputQuery': function (e) {
+        // catch enter
+        if (e.keyCode == 13) {
+            executeQuery();
+            return false;
+        }
+    },
+
     'click #btnExecuteQuery': function (e) {
-        var connection = Connections.findOne({_id: Session.get(strSessionConnection)});
-        var selectedCollection = Session.get(strSessionSelectedCollection);
-        var query = 'db.' + selectedCollection + '.' + $('#inputQuery').val();
-
-        Meteor.call('executeQuery', connection, query, function (err, result) {
-            if (result.error) {
-                toastr.error("Couldn't execute query: " + result.error.message);
-                return;
-            }
-
-            getEditor().set(result.result);
-        });
+        executeQuery();
     }
-
 });
+
+executeQuery = function () {
+    var connection = Connections.findOne({_id: Session.get(strSessionConnection)});
+    var selectedCollection = Session.get(strSessionSelectedCollection);
+    var query = 'db.' + selectedCollection + '.' + $('#inputQuery').val();
+
+    Meteor.call('executeQuery', connection, query, function (err, result) {
+        if (result.error) {
+            toastr.error("Couldn't execute query: " + result.error.message);
+            return;
+        }
+
+        getEditor().set(result.result);
+
+        AceEditor.instance("aceeditor", {
+            mode: "javascript",
+            theme: 'dawn'
+        }, function (editor) {
+            editor.$blockScrolling = Infinity;
+            editor.setOptions({
+                fontSize: "12pt"
+            });
+            editor.setValue(JSON.stringify(result.result, null, '\t'));
+        });
+
+    });
+}
