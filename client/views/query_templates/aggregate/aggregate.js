9ac753452dc7eba1f1cd440095c30fb8d8fdd233
Sercan <ozdemirsercan27@gmail.com>
2016-01-02 13:40:30 +0200

    aggregate implemented

---
+++ client/views/query_templates/aggregate/aggregate.js
@@ -0,0 +1,37 @@
+/**
+ * Created by RSercan on 2.1.2016.
+ */
+Template.aggregate.onRendered(function () {
+    Template.aggregate.initializeAceEditor();
+});
+
+Template.aggregate.initializeAceEditor = function () {
+    AceEditor.instance('acePipeline', {
+        mode: "javascript",
+        theme: 'dawn'
+    }, function (editor) {
+        editor.$blockScrolling = Infinity;
+        editor.setOptions({
+            fontSize: "11pt",
+            showPrintMargin: false
+        });
+    });
+};
+
+Template.aggregate.executeQuery = function () {
+    Template.browseCollection.initExecuteQuery();
+    var connection = Connections.findOne({_id: Session.get(Template.strSessionConnection)});
+    var selectedCollection = Session.get(Template.strSessionSelectedCollection);
+    var pipeline = ace.edit("acePipeline").getSession().getValue();
+
+    pipeline = Template.convertAndCheckJSON(pipeline);
+    if (pipeline["ERROR"]) {
+        toastr.error("Syntax error on selector: " + pipeline["ERROR"]);
+        Ladda.stopAll();
+        return;
+    }
+
+    Meteor.call("aggregate", connection, selectedCollection, pipeline, function (err, result) {
+        Template.renderAfterQueryExecution(err, result);
+    });
+};
--- server/mongodb_methods.js
+++ server/mongodb_methods.js
@@ -42,6 +42,34 @@ Meteor.methods({
         return proceedFindOneAndModifyQuery(connection, selectedCollection, selector, null, options, 'Delete');
     },
 
+    'aggregate': function (connection, selectedCollection, pipeline) {
+        var connectionUrl = getConnectionUrl(connection);
+        console.log('executing aggregate query ' + JSON.stringify(pipeline) + ' on: ' + connectionUrl + '/' + selectedCollection);
+
+        var mongodbApi = Meteor.npmRequire('mongodb').MongoClient;
+
+        convertJSONtoBSON(pipeline);
+
+        var result = Async.runSync(function (done) {
+            mongodbApi.connect(connectionUrl, function (err, db) {
+                try {
+                    db.collection(selectedCollection).aggregate(pipeline, function (err, count) {
+                        done(err, count);
+                        db.close();
+                    });
+                }
+                catch (ex) {
+                    console.error(ex);
+                    done(ex, null);
+                    db.close();
+                }
+            });
+        });
+
+        convertBSONtoJSON(result);
+        return result;
+    },
+
     'count': function (connection, selectedCollection, selector) {
         var connectionUrl = getConnectionUrl(connection);
         console.log('executing count query ' + JSON.stringify(selector) + ' on: ' + connectionUrl + '/' + selectedCollection);
@@ -166,4 +194,8 @@ var proceedFindQuery = function (connection, selectedCollection, selector, curso
 
     convertBSONtoJSON(result);
     return result;
+};
+
+var proceedQueryExecution = function (connection, selectedCollection) {
+
 };
