eb6ab33226541cbd2a55dfd4c2c321e4de98cec8
Birk Skyum <birkskyum@gmail.com>
2016-08-07 13:08:05 +0200

    Load addons (eg. autocomplete) and theme for codemirror

--- client/views/pages/edit_document/edit_document.js
+++ client/views/pages/edit_document/edit_document.js
@@ -1,5 +1,17 @@
 var toastr = require('toastr');
 var CodeMirror = require("codemirror");
+
+require("/node_modules/codemirror/mode/javascript/javascript.js");
+require("/node_modules/codemirror/addon/fold/brace-fold.js");
+require("/node_modules/codemirror/addon/fold/comment-fold.js");
+require("/node_modules/codemirror/addon/fold/foldcode.js");
+require("/node_modules/codemirror/addon/fold/foldgutter.js");
+require("/node_modules/codemirror/addon/fold/indent-fold.js");
+require("/node_modules/codemirror/addon/fold/markdown-fold.js");
+require("/node_modules/codemirror/addon/fold/xml-fold.js");
+require("/node_modules/codemirror/addon/hint/javascript-hint.js");
+require("/node_modules/codemirror/addon/hint/show-hint.js");
+
 var Ladda = require('ladda');
 
 /**
