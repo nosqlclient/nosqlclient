464e060dca7c521c9e6d50cb66391d468f7d7ac9
rsercano <ozdemirsercan27@gmail.com>
2016-12-10 09:08:09 +0300

    #253

--- client/imports/views/query_templates/admin/command/command.js
+++ client/imports/views/query_templates/admin/command/command.js
@@ -2,22 +2,46 @@ import {Template} from 'meteor/templating';
 import {Meteor} from 'meteor/meteor';
 import Helper from '/client/imports/helper';
 import {initExecuteQuery} from '/client/imports/views/pages/admin_queries/admin_queries';
+import Enums from '/lib/imports/enums';
 
+import '/client/imports/views/query_templates_options/max_time_ms/max_time_ms.html';
 import './command.html';
 
-var toastr = require('toastr');
-var Ladda = require('ladda');
+const toastr = require('toastr');
+const Ladda = require('ladda');
 /**
  * Created by RSercan on 10.1.2016.
  */
 Template.command.onRendered(function () {
     Helper.initializeCodeMirror($('#divCommand'), 'txtCommand');
     Helper.changeRunOnAdminOptionVisibility(true);
+
+    initializeOptions();
 });
+
+const initializeOptions = function () {
+    let cmb = $('#cmbCommandOptions');
+    $.each(Helper.sortObjectByKey(Enums.COMMAND_OPTIONS), function (key, value) {
+        cmb.append($("<option></option>")
+            .attr("value", key)
+            .text(value));
+    });
+
+    cmb.chosen();
+    Helper.setOptionsComboboxChangeEvent(cmb);
+};
 
 Template.command.executeQuery = function () {
     initExecuteQuery();
-    var command = Helper.getCodeMirrorValue($('#divCommand'));
+    let command = Helper.getCodeMirrorValue($('#divCommand'));
+    let options = {};
+
+    if ($.inArray("MAX_TIME_MS", Session.get(Helper.strSessionSelectedOptions)) != -1) {
+        let maxTimeMsVal = $('#inputMaxTimeMs').val();
+        if (maxTimeMsVal) {
+            options[Enums.COMMAND_OPTIONS.MAX_TIME_MS] = parseInt(maxTimeMsVal);
+        }
+    }
 
     command = Helper.convertAndCheckJSON(command);
     if (command["ERROR"]) {
@@ -26,9 +50,9 @@ Template.command.executeQuery = function () {
         return;
     }
 
-    var runOnAdminDB = $('#aRunOnAdminDB').iCheck('update')[0].checked;
+    let runOnAdminDB = $('#aRunOnAdminDB').iCheck('update')[0].checked;
 
-    Meteor.call("command", command, runOnAdminDB, function (err, result) {
+    Meteor.call("command", command, runOnAdminDB, options, function (err, result) {
             Helper.renderAfterQueryExecution(err, result, true);
         }
     );
