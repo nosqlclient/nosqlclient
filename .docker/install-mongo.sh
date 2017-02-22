08e67ccfcbc2440b245eb01429bebd4525f79176
Mischa Holz <me@mhlz.de>
2017-02-22 13:33:38 +0100

    Fix mongo installation

--- .docker/install-mongo.sh
+++ .docker/install-mongo.sh
@@ -8,9 +8,9 @@ fi
 
   printf "\n[-] Installing MongoDB ${MONGO_VERSION}...\n\n"
 
-	apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 0C49F3730359A14518585931BC711F9BA15703C6
-
-  echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/$MONGO_MAJOR main" > /etc/apt/sources.list.d/mongodb-org.list
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+	
+	echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/$MONGO_MAJOR multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org.list
 
 	apt-get update
 
