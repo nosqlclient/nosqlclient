f94d74d96eff70b4f4dc307a2d8965b9baa07b13
rsercano <ozdemirsercan27@gmail.com>
2017-05-08 10:03:07 +0200

    keyserver for gpg

--- .docker/install-deps.sh
+++ .docker/install-deps.sh
@@ -15,7 +15,7 @@ wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/downloa
 
 export GNUPGHOME="$(mktemp -d)"
 
-gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
+gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
 gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu
 
 rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc
