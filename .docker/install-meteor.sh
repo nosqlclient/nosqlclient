efaf43394e966c12cfba9a913da3f36294ac8c66
rsercano <ozdemirsercan27@gmail.com>
2017-05-08 13:22:08 +0200

    trying to fix tar overlayfs issue

--- .docker/install-meteor.sh
+++ .docker/install-meteor.sh
@@ -7,7 +7,7 @@ set -e
 
   # change tar command in the install script with bsdtar ( bsdtar -xf "$TARBALL_FILE" -C "$INSTALL_TMPDIR" )
   printf "\n[-] Changing tar with bsdtar...\n\n"
-  sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_test.sh
+  sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_meteor.sh
 
   # install
   printf "\n[-] Installing latest Meteor ...\n\n"
