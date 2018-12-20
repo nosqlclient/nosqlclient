90adf042c3d6442e07800e00d7add38574c985f1
Muharrem Tigdemir <muharrem.tigdemir@growthengineering.co.uk>
2018-12-20 15:12:32 +0000

    Formatting Header.js

--- client/src/views/layout/Header.js
+++ client/src/views/layout/Header.js
@@ -1,27 +1,50 @@
-import React, { Component } from 'react';
-import { AppSidebarToggler, AppHeaderDropdown } from '@coreui/react';
-import { DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
+import React, { Component } from "react";
+import { AppSidebarToggler, AppHeaderDropdown } from "@coreui/react";
+import {
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
 
 class Header extends Component {
   render() {
     return (
       <React.Fragment>
-        <AppSidebarToggler className='d-lg-none' display='md' mobile />
-        <AppSidebarToggler className='d-md-down-none' display='lg' />
+        <AppSidebarToggler className="d-lg-none" display="md" mobile />
+        <AppSidebarToggler className="d-md-down-none" display="lg" />
         <Nav className="ml-auto" navbar>
           <AppHeaderDropdown direction="down">
             <DropdownToggle nav>
-              <i className="fa fa-chevron-circle-down"></i> More
+              <i className="fa fa-chevron-circle-down" />
+              More
             </DropdownToggle>
-            <DropdownMenu right style={{ right: 'auto' }}>
-              <DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
-              <DropdownItem><i className="fa fa-download"></i> Import Nosqlclient Data</DropdownItem>
-              <DropdownItem><i className="fa fa-upload"></i> Export Nosqlclient Data</DropdownItem>
-              <DropdownItem><i className="fa fa-globe"></i> About</DropdownItem>
+            <DropdownMenu right style={{ right: "auto" }}>
+              <DropdownItem>
+                <i className="fa fa-wrench" />
+                Settings
+              </DropdownItem>
+              <DropdownItem>
+                <i className="fa fa-download" />
+                Import Nosqlclient Data
+              </DropdownItem>
+              <DropdownItem>
+                <i className="fa fa-upload" />
+                Export Nosqlclient Data
+              </DropdownItem>
+              <DropdownItem>
+                <i className="fa fa-globe" />
+                About
+              </DropdownItem>
             </DropdownMenu>
           </AppHeaderDropdown>
           <NavItem className="px-3">
-            <NavLink href="#"><i className="fa fa-sign-in"></i> Connect</NavLink>
+            <NavLink href="#">
+              <i className="fa fa-sign-in" />
+              <strong>Connect</strong>
+            </NavLink>
           </NavItem>
         </Nav>
       </React.Fragment>
