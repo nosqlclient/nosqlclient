2ccccab2c41921579073e56a512a4e576fd8fe4d
sercan <sercan@pribas.com>
2018-12-21 09:26:02 +0100

    initial graphql implementation #464

--- client/src/views/yield/Dashboard.js
+++ client/src/views/yield/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Col, Row } from 'reactstrap';
+import { Query } from 'react-apollo';
+import Communicator, { createQuery } from '../../modules/communicator';
 
 class Dashboard extends Component {
   render() {
@@ -7,7 +9,25 @@ class Dashboard extends Component {
       <div className="animated fadeIn">
         <Row>
           <Col xs="12" sm="12" lg="12">
-            BOK
+            <Query query={createQuery(Communicator.queries.getBooks(['author', 'title', 'id']))}>
+              {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) {
+                  return (
+                    <p>
+                      Error:
+                      {JSON.stringify(error)}
+                    </p>
+                  );
+                }
+
+                return data.allBooks.map(({id, author, title }) => (
+                  <div key={id}>
+                    <p>{`${id}: ${author}: ${title}`}</p>
+                  </div>
+                ));
+              }}
+            </Query>
           </Col>
         </Row>
 
