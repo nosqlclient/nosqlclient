e90386f451e2cf7383b5aed9f7e4cccca96d1bbb
sercan <sercan@pribas.com>
2018-12-21 12:26:15 +0100

    formatting #464

--- client/src/views/layout/Connect.js
+++ client/src/views/layout/Connect.js
@@ -8,40 +8,40 @@ class Connect extends Component {
     this.state = { show: true };
   }
 
-    toggleModal = () => {
-      this.setState({ show: !this.state.show });
-    }
+  toggleModal = () => {
+    this.setState({ show: !this.state.show });
+  }
 
-    render() {
-      return (
-        <Modal isOpen={this.state.show} className="modal-lg">
-          <ModalHeader toggle={this.toggleModal}>
-            <i className="fa fa-laptop modal-icon" />
-            <h1 className="modal-title w-100"> Connections</h1>
-            <small>You can either connect an existing connection or create a new one.</small>
-          </ModalHeader>
-          <ModalBody>
-            <Table responsive>
-              <thead>
-                <tr>
-                  <th>Connection Name</th>
-                  <th>Servers</th>
-                  <th>Properties</th>
-                  <th>Edit</th>
-                  <th>Clone</th>
-                  <th>Delete</th>
-                </tr>
-              </thead>
-            </Table>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggleModal}>Connect Now</Button>
-            {' '}
-            <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      );
-    }
+  render() {
+    return (
+      <Modal isOpen={this.state.show} className="modal-lg">
+        <ModalHeader toggle={this.toggleModal}>
+          <i className="fa fa-laptop modal-icon" />
+          <h1 className="modal-title w-100"> Connections</h1>
+          <small>You can either connect an existing connection or create a new one.</small>
+        </ModalHeader>
+        <ModalBody>
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>Connection Name</th>
+                <th>Servers</th>
+                <th>Properties</th>
+                <th>Edit</th>
+                <th>Clone</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+          </Table>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.toggleModal}>Connect Now</Button>
+          {' '}
+          <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
 }
 
 export default Connect;
