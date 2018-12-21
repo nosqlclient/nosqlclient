1b903a52072ff7d3bded6174907eb1748c7ab27d
sercan <sercan@pribas.com>
2018-12-21 07:04:26 +0100

    initial graphql implementation, refactored some folder names #464

--- client/src/views/yield/Dashboard.js
+++ client/src/views/yield/Dashboard.js
@@ -4,9 +4,9 @@ import { Col, Row } from 'reactstrap';
 class Dashboard extends Component {
   render() {
     return (
-      <div className='animated fadeIn'>
+      <div className="animated fadeIn">
         <Row>
-          <Col xs='12' sm='12' lg='12'>
+          <Col xs="12" sm="12" lg="12">
             BOK
           </Col>
         </Row>
