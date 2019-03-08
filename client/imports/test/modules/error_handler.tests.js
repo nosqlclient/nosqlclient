6c162784a5b9b608aab0badb693fe31c39d0b014
rsercano <ozdemirsercan27@gmail.com>
2019-03-08 11:43:54 +0100

    #480 error handler tests implemented

--- client/imports/test/modules/error_handler.tests.js
+++ client/imports/test/modules/error_handler.tests.js
@@ -5,9 +5,18 @@ import { expect } from 'meteor/practicalmeteor:chai';
 import Helper from '/client/imports/helpers/helper';
 import sinon from 'sinon';
 
+/**
+ * showMeteorFuncError will be tested in Notification tests since it's only a proxy.
+ */
 describe('ErrorHandler', () => {
+  const reason = 'myReason';
+  const detailsMessage = 'detailsMessage';
+
   beforeEach(() => {
-    sinon.stub(Helper, 'translate').returns('dummy message');
+    sinon.stub(Helper, 'translate')
+      .withArgs(sinon.match({ key: reason })).returns(reason)
+      .withArgs(sinon.match({ key: detailsMessage }))
+      .returns(detailsMessage);
   });
 
   afterEach(() => {
@@ -15,15 +24,104 @@ describe('ErrorHandler', () => {
   });
 
   describe('getErrorMessage tests', () => {
-    it('expected behaviour', () => {
+    it('error filled, result is null', () => {
       // prepare
 
       // execute
-      const errorMessage = ErrorHandler.getErrorMessage({ reason: 'myReason' }, null, null);
+      const errorMessage = ErrorHandler.getErrorMessage({ error: '1009', reason }, null, null);
 
-      console.log(errorMessage);
       // verify
-      expect(errorMessage).to.equal('dummy message: myReason');
+      expect(errorMessage).to.equal(`[1009] ${reason}`);
+    });
+
+    it('error filled with details but no message, result is null', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage({ error: '1009', details: {}, reason }, null, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${reason}`);
+    });
+
+    it('error filled with details, result is null', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage({ error: '1009', details: { message: detailsMessage }, reason }, null, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${detailsMessage}`);
+    });
+
+    it('error filled with details, result filled (error has prio)', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage({ error: '1009', details: { message: detailsMessage }, reason }, { error: { message: reason } }, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${detailsMessage}`);
+    });
+
+    it('error filled without details, result filled (error has prio)', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage({ error: '1009', reason }, { error: { message: detailsMessage } }, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${reason}`);
+    });
+
+    it('result filled without details, error is null', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage(null, { error: { error: '1009', message: detailsMessage } }, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${detailsMessage}`);
+    });
+
+    it('result filled with details (details has prio), error is null', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage(null, { error: { error: '1009', message: reason, details: { message: detailsMessage } } }, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${detailsMessage}`);
+    });
+
+    it('result filled with wrong details, error is null', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage(null, { error: { error: '1009', message: reason, details: { msg: detailsMessage } } }, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${reason}`);
+    });
+
+    it('result filled with message & errmsg (message has prio), error is null', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage(null, { error: { error: '1009', message: reason, errMsg: detailsMessage } }, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${reason}`);
+    });
+
+    it('result filled with errmsg, error is null', () => {
+      // prepare
+
+      // execute
+      const errorMessage = ErrorHandler.getErrorMessage(null, { error: { error: '1009', errmsg: detailsMessage } }, null);
+
+      // verify
+      expect(errorMessage).to.equal(`[1009] ${detailsMessage}`);
     });
   });
 });
