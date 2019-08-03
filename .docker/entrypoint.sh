b1502f17e4915c765598901712f89a03bcd6032a
Derrick Burns <derrickrburns@gmail.com>
2019-08-02 17:03:53 -0700

    Allow parts of Mongo connection string to be assembled.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -28,12 +28,57 @@ file_env() {
 	unset "$fileVar"
 }
 
+file_env 'MONGO_SCHEME'
+file_env 'MONGO_USERNAME'
+file_env 'MONGO_PASSWORD'
+file_env 'MONGO_ADDRESSES'
+file_env 'MONGO_OPT_PARAMS'
+file_env 'MONGO_DATABASE'
+file_env 'MONGO_TLS'
 file_env 'MONGO_URL'
+
+MONGO_SCHEME=${MONGO_SCHEME:-mongodb}
+MONGO_ADDRESSES=${MONGO_ADDRESSES:-127.0.0.1:27017}
+MONGO_DATABASE=${MONGO_DATABASE:-meteor}
+
+URL=
+if [ "${MONGO_URL}" == "" ]
+then
+    URL=${MONGO_SCHEME}://
+    if [ ! -z ${MONGO_USERNAME+x} ]
+    then
+        URL=${URL}${MONGO_USERNAME}
+        if [ ! -z ${MONGO_PASSWORD+x} ]
+    	then
+    	    URL=${URL}:${MONGO_PASSWORD}
+    	fi
+        URL=${URL}@
+    fi
+    URL=${URL}${MONGO_ADDRESSES}/${MONGO_DATABASE}
+	if [ -z ${MONGO_TLS+x} ]
+	then
+    	URL="${URL}?ssl=false"
+	else
+	    URL="${URL}?ssl=${MONGO_TLS}"
+	fi
+    if [ ! -z ${MONGO_OPT_PARAMS+x} ]
+	then  
+	    URL="${URL}&${MONGO_OPT_PARAMS}"
+	fi
+	MONGO_URL=${URL}
+fi
+
 file_env 'MONGOCLIENT_DEFAULT_CONNECTION_URL'
+
+if [ "${MONGOCLIENT_DEFAULT_CONNECTION_URL}" == "" ]
+then
+	MONGOCLIENT_DEFAULT_CONNECTION_URL=$URL
+fi
+
 file_env 'MONGOCLIENT_USERNAME'
 file_env 'MONGOCLIENT_PASSWORD'
 
-# Defualt connection URL in case no connection URL is provided using the environment variables
+# Default connection URL in case no connection URL is provided using the environment variables
 MONGO_URL=${MONGO_URL:-mongodb://127.0.0.1:27017/meteor}
 
 # try to start local MongoDB if no external MONGO_URL was set
